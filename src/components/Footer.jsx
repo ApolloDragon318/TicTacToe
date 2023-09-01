bfafdb8bcd64287d883b64900d416f7317094dc8
Apollo Dragon <apollodragon.soft@gmail.com>
2023-08-31 19:33:35 -0500

    complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer-container'>
+            
+            <p>@DevenCarson</p>
+
+            <div className='logos'>
+                <a target='_blank' href="https://www.github.com/apollodragon318">
+                    <i className='bx bxl-github' ></i>
+                </a>
+
+                <a target='_blank' href="https://www.linkedin.com/in/deven-carson-dc94/">
+                    <i className='bx bxl-linkedin-square' ></i>
+                </a>
+            </div>
+
+            
+        </footer>
+    )
+}
+
+export default Footer
